ec37c1fd90c71d731e88f8115cef6bdbc2d51cea
danish45007 <danish45007@gmail.com>
2020-09-01 01:20:40 +0530

    header component added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,7 @@ import "./Header.css";
 import {Avatar } from '@material-ui/core';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
@@ -16,6 +17,7 @@ function Header() {
                 <AccessTimeIcon />
             </div>
             <div className="header__search">
+                <SearchIcon />
                 <input 
                 placeholder="Search UnNamed Developers"
                 type="text"
