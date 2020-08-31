ebe78c5f6266ba01416cf3238bf3fc5c53ba29b4
danish45007 <danish45007@gmail.com>
2020-08-31 21:12:50 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header/Header';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./Header.css";
+import {Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <Avatar 
+                className="header__avatar"
+                // alt={user?.displayName}
+                // src={user?.photoUrl}
+                />
+                <AccessTimeIcon />
+            </div>
+            <div className="header__search">
+                <input 
+                placeholder="Search UnNamed Developers"
+                type="text"
+                />
+            </div>
+            <div className="header__right">
+                <HelpOutlineIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Header
