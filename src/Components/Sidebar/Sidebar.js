641cbb3e47f2d154d5651ed16021e1bbebcab437
danish45007 <danish45007@gmail.com>
2020-09-04 16:53:29 +0530

    sidebar option component

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -2,7 +2,8 @@ import React from 'react';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
 import "./Sidebar.css";
-
+import SidebarOption from '../SidebarOptions/SidebarOption';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
 
 function Sidebar() {
     return (
@@ -18,6 +19,12 @@ function Sidebar() {
                 <div className="create">
                     <CreateIcon />
                 </div>
+            </div>
+                {/* Sidebar Option Components takes props as Icon as
+                component and title  */}
+            <div>
+                <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+                <SidebarOption title="backend" />
             </div>
         </div>
     )
---
+++ src/Components/SidebarOptions/SidebarOption.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './SidebarOption.css';
+
+function SidebarOption( {Icon,title} ) {
+    return (
+        <div className="sidebarOption">
+            {Icon && <Icon className="sidebarOption__icon" />}
+            
+            {/* Icon exists then normal clickable options and if Icon doesn't exists title as Channel name */}
+            
+            {Icon ? 
+                <h3>{title}</h3> : (
+                <h3>
+                    <span className="sidebarOption__hash"># </span>
+                    {title}
+                </h3>
+                )}
+        </div>
+    );
+}
+
+export default SidebarOption
