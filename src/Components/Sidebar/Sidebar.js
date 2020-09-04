50ce14a5e35ec7310f924c75134a23a6ed4d143a
danish45007 <danish45007@gmail.com>
2020-09-05 02:17:54 +0530

    pulling channel data from firestore

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
 import "./Sidebar.css";
@@ -13,8 +13,21 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import db from '../../Firebase';
 
 function Sidebar() {
+
+    const [channels, setChannels] = useState([]);
+
+    useEffect(() => {
+        db.collection("rooms").onSnapshot((snapshot) => (
+            setChannels(snapshot.docs.map(doc => ({
+                id: doc.id,
+                name: doc.data().name
+            })))
+        ))
+    }, [])
+
     return (
         <div className="sidebar">
             <div className="sidebar__header">
@@ -44,6 +57,10 @@ function Sidebar() {
                 <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
                 <hr className="hr" />
                 <SidebarOption Icon={AddIcon} title="Add channels" />
+                {console.log(channels)}
+                {channels.map(channel => (
+                    <SidebarOption title={channel.name} />
+                ))}
             </div>
         </div>
     )
